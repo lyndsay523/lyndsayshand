20a60fada9b9e2f89fe7082f062ba0723b370494
lyndsay523 <lyndsay.es@gmail.com>
2021-06-17 11:24:14 -0600

    new site

--- installtheme_ex.R
+++ installtheme_ex.R
@@ -1,2 +1,2 @@
-blogdown::install_theme(theme="MarcusVirg/forty",theme_example=TRUE,update_config = FALSE)
-blogdown::new_site(theme = "MarcusVirg/forty")
+blogdown::install_theme(theme="nanxstats/hugo-tanka",theme_example=TRUE,update_config = FALSE)
+blogdown::new_site(theme = "nanxstats/hugo-tanka")
